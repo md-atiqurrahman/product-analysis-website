07f4cfc8b084dc56e71e2ef07b9e7769545a2c99
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-04-05 22:50:20 +0600

    add chart

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
+    const [income, setIncome] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setIncome(data))
+    }, [])
     return (
-        <div>
-            <h2>this is DashBoard</h2>
+        <div className=''>
+            <div className='mt-16'>
+                <LineChart width={730} height={250} data={income}>
+                    <Line dataKey='investment' stroke="#8884d8"></Line>
+                    <Line dataKey='sell' stroke='#ff7300'></Line>
+                    <Line dataKey='revenue' stroke="#82ca9d"></Line>
+                    <XAxis dataKey='month'></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <div className='mt-16'>
+                <BarChart width={730} height={250} data={income}>
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="sell" fill="#ff7300" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                    <XAxis dataKey='month'></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </BarChart>
+            </div>
         </div>
     );
 };
